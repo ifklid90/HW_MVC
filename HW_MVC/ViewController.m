6cf64319bfb33309e2f1c47d7fa59cb75e74531a
Airat <mulukov.air@gmail.com>
2014-12-28 19:27:43 +0300

    1

--- HW_MVC/ViewController.m
+++ HW_MVC/ViewController.m
@@ -9,7 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, weak) IBOutlet UIButton *btn;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segment;
+@property (weak, nonatomic) IBOutlet UISwitch *mySwitch;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 @end
 
 @implementation ViewController
@@ -17,11 +21,65 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *str = [[NSUserDefaults standardUserDefaults] objectForKey:@"textFieldValue"];
+    if (str)
+        self.textField.text = str;
+    
+    NSNumber *check = [[NSUserDefaults standardUserDefaults] objectForKey:@"switchValue"];
+    check = check ? check : @0;
+    [self.mySwitch setOn:check.boolValue animated:NO];
+    
+    NSNumber *select = [[NSUserDefaults standardUserDefaults] objectForKey:@"selectValue"];
+    select = select ? select : @0;
+    self.segment.selectedSegmentIndex = select.integerValue;
+    
+    NSNumber *sliderValue = [[NSUserDefaults standardUserDefaults] objectForKey:@"sliderValue"];
+    sliderValue = sliderValue ? sliderValue : @10;
+    self.slider.value = sliderValue.floatValue;
+    
+    NSData *color = [[NSUserDefaults standardUserDefaults] objectForKey:@"buttonColorValue"];
+    if (color)
+        [self.btn setTintColor:[NSKeyedUnarchiver unarchiveObjectWithData:color]];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)buttonPressed:(id)sender {
+    UIColor *color = [self randColor];
+    [self.btn setTintColor:color];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:[NSKeyedArchiver archivedDataWithRootObject:color] forKey:@"buttonColorValue"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+- (IBAction)segmentChanged:(id)sender {
+    [[NSUserDefaults standardUserDefaults] setObject:[[NSNumber alloc] initWithInteger:self.segment.selectedSegmentIndex] forKey:@"segmentValue"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+- (IBAction)switchChanged:(id)sender {
+    [[NSUserDefaults standardUserDefaults] setObject:[[NSNumber alloc] initWithBool:self.mySwitch.isOn] forKey:@"switchValue"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+- (IBAction)sliderChanged:(id)sender {
+    [[NSUserDefaults standardUserDefaults] setObject:[[NSNumber alloc] initWithFloat:self.slider.value] forKey:@"sliderValue"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+- (IBAction)textFieldChanged:(id)sender {
+    [[NSUserDefaults standardUserDefaults] setObject:self.textField.text forKey:@"textFieldValue"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (UIColor *)randColor
+{
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+    return color;
 }
 
 @end
