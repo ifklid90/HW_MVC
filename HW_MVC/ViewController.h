2725a82b65d8d27f45c17822a57ef0575f369565
Airat <mulukov.air@gmail.com>
2014-12-28 18:11:38 +0300

    firs commit

---
+++ HW_MVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW_MVC
+//
+//  Created by User on 28.12.14.
+//  Copyright (c) 2014 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HW_MVC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HW_MVC
+//
+//  Created by User on 28.12.14.
+//  Copyright (c) 2014 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
